475daae10fb230cf0d0dddee65498ef5cefbf015
JulitKuchta <julita.kuchta@student.pk.edu.pl>
2025-10-28 13:28:54 +0100

    4.1.

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,49 @@
-const express = require('express');
+const express = require("express");
 
-const adminController = require('../controllers/admin');
+const { check, body } = require("express-validator");
+
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-router.get('/products', adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
-router.get('/add-product', adminController.getAddProduct );
+router.get("/add-product", adminController.getAddProduct);
 
-router.post('/add-product', adminController.postAddProduct );
+router.post(
+  "/add-product",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 characters long."),
+    body("price").isNumeric().withMessage("Price must be a valid number."),
+    body("description")
+      .trim()
+      .isLength({ min: 5, max: 400 })
+      .withMessage("Description must be between 5 and 400 characters long."),
+  ],
+  adminController.postAddProduct
+);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.post('/edit-product',  adminController.postEditProduct );
-    
-router.delete('/product/:productId', adminController.deleteProduct);
+router.post(
+  "/edit-product",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 characters long."),
+    body("price").isNumeric().withMessage("Price must be a valid number."),
+    body("description")
+      .trim()
+      .isLength({ min: 5, max: 400 })
+      .withMessage("Description must be between 5 and 400 characters long."),
+  ],
+  adminController.postEditProduct
+);
+
+router.delete("/product/:productId", adminController.deleteProduct);
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 const adminController = require("../controllers/admin");
@@ -8,7 +9,37 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Valid e-mail address required.")
+      .custom(async (value, { req }) => {
+        const emailExists = await User.findOne({ email: value });
+        if (emailExists) {
+          return Promise.reject("Email in use. Different email required.");
+        }
+      })
+      .normalizeEmail(),
+    body(
+      "password",
+      "Password must contains chars and digits, minimum length 5."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postSignup
+);
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
